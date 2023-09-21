9ecea05d12043fb77efc146beefe30383f422ccf
листик <119734607+LiztochekCode@users.noreply.github.com>
2023-09-21 17:23:38 +0300

    add some folders

---
+++ modules/rpc.py
@@ -0,0 +1,85 @@
+from configparser import ConfigParser
+from pypresence import Presence
+from modules.yandexmusic import MYAPI
+import time
+
+config = ConfigParser()
+
+config.read('info/config.ini')
+
+dRPC = Presence(client_id=config.get('main', 'ds'))
+dRPC.connect()
+
+class MRPC:
+    def Clear():
+        dRPC.clear()
+
+    def idling():
+        dRPC.update(
+            details="Ничего не слушает",
+            large_image="https://github.com/liztochekcode/yandexmusicpythonrpc/raw/main/fallback-black_2.gif",
+            small_image="https://github.com/liztochekcode/yandexmusicpythonrpc/blob/main/logo.png?raw=true",
+            large_text=f"Ничего не прослушивается."
+        )
+
+    def updatePresence(artist = None, 
+                    song = None, 
+                    image_link = None,
+                    song_link = None):
+        btns = [
+            {
+                "label": "Слушать",
+                "url": song_link
+            },
+        ] if not song_link is None else None
+
+        dRPC.update(
+            details=song,
+            state=artist,
+            large_image=image_link,
+            small_image="https://github.com/liztochekcode/yandexmusicpythonrpc/blob/main/logo.png?raw=true",
+            large_text=f"{artist if not artist is None else ''} - {song}",
+            buttons=btns
+        )
+
+    def ForceUpdate():
+        try:
+            song = MYAPI.get_current_track()
+            if song['id'] != lasttrack:
+                lasttrack = song['id']
+                switch = 1
+            if switch == 1:
+                switch = 0
+                #print(f"[Яндекс Музыка] Слушаем {artist} - {song}")
+                MRPC.updatePresence(
+                    song['artist'],
+                    song['title'],
+                    song['image'],
+                    song['link']
+                )
+        except Exception as e:
+            print(e)
+            MRPC.idling()
+
+    def callPresence():
+        while True:
+            switch = 0
+            lasttrack = 0
+            try:
+                song = MYAPI.get_current_track()
+                if song['id'] != lasttrack:
+                    lasttrack = song['id']
+                    switch = 1
+                if switch == 1:
+                    switch = 0
+                    #print(f"[Яндекс Музыка] Слушаем {artist} - {song}")
+                    MRPC.updatePresence(
+                        song['artist'],
+                        song['title'],
+                        song['image'],
+                        song['link']
+                    )
+            except Exception as e:
+                print(e)
+                MRPC.idling()            
+            time.sleep(1)
---
+++ modules/yandexmusic.py
@@ -0,0 +1,57 @@
+from configparser import ConfigParser
+from yandex_music import Client
+from modules.getToken import token
+from PyQt6.QtWidgets import QMessageBox
+
+config = ConfigParser()
+
+config.read('info/config.ini')
+
+if len(config.get("main","ym")) <= 2:
+    print("[Яндекс Музыка] Замечен первый запуск, для работы приложения войдите в аккаунт яндекса, в открывшемся окне.")
+    config.set("main", "ym", token.get_token())
+    print("[Яндекс Музыка] Успешный запуск")
+    with open("info/config.ini", "w") as config_file:
+        config.write(config_file)
+    
+    client = Client(config.get("main", "ym")).init()
+else:
+    print("[Яндекс Музыка] Успешный запуск")
+    client = Client(config.get("main", "ym")).init()
+
+
+class MYAPI:
+    def ForceUpdateToken():
+        config.set("main", "ym", token.get_token())
+        print("[Яндекс Музыка] Успешный запуск")
+        with open("info/config.ini", "w") as config_file:
+            config.write(config_file)
+            
+        client = Client(config.get("main", "ym")).init()
+
+    def get_current_track():
+        queue = client.queues_list()
+        if len(queue) == 0:
+            raise "nothing playing"
+
+        queue = client.queue(queue[0].id)
+        if queue.context.id == 'user:onyourwave':
+            return {
+                "title" : "Моя волна",
+                "link" : None,
+                "image" : "https://github.com/liztochekcode/yandexmusicpythonrpc/raw/main/fallback-black_2.gif",
+                "id" : "wave",
+                "artist" : None
+            }
+            
+        track = queue.get_current_track().fetch_track()
+
+        return {
+            "title" : track.title,
+            "link" : f"https://music.yandex.ru/album/{track['albums'][0]['id']}/track/{track['id']}/",
+            "image" : f"https://{track.cover_uri.replace('%%', '1000x1000')}",
+            "id" : track.id,
+            "artist" : ", ".join(track.artists_name())
+        }
+
+
